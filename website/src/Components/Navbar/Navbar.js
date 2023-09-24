b1fc00e9f35d623b9f03f1215a9b1f3d5458d6d1
aleksa <zabumaphu@gmail.com>
2023-09-24 01:50:41 -0400

    Navbar complete.

--- website/src/Components/Navbar/Navbar.js
+++ website/src/Components/Navbar/Navbar.js
@@ -1,19 +1,28 @@
+import {useRef} from "react";
 import {FaBars, FaTimes} from "react-icons/fa";
+import"./Navbar.css";
 
 function Navbar() {
+    const navRef = useRef();
+    const showNavBar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     return (
         <header>
-            <h3>Logo</h3>
-            <nav>
+            <h3>Welcome to Aleksa's Site</h3>
+            <nav ref={navRef}>
                 <a href ="/#">Home</a>
                 <a href ="/#">My Work</a>
                 <a href ="/#">Blog</a>
                 <a href ="/#">About Me</a>
-                <button>
+                <button className="nav-btn nav-close-btn" onClick={showNavBar}>
                     <FaTimes/>
                 </button>
             </nav>
-            <FaBars/>
+            <button className="nav-btn" onClick={showNavBar}>
+                <FaBars/>
+            </button>
         </header>
     );
 }
