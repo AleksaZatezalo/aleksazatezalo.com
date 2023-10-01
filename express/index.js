42a89e8ce262c187b59048ca7ddc77e357f92859
aleksa <zabumaphu@gmail.com>
2023-10-01 19:34:28 -0400

    Began Authentication/.

--- express/index.js
+++ express/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors);
+app.use(express.json());
 
 app.post('/register', (req,res) => {
-    res.json('test ok3');
+    const {username, password} = req.body;
+    res.json({requestData:{usename,password}});
 })
 
 app.listen(4000)
