06d49cf1c5218d53b450aef06795ae54bc24f28d
aleksa <zabumaphu@gmail.com>
2023-10-02 01:34:12 -0400

    Requests began to mongo db. Git DB privitized.

--- express/index.js
+++ express/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+const User = require('./modules/user')
 const cors = require('cors');
 
 app.use(cors);
 app.use(express.json());
 
-app.post('/register', (req,res) => {
+mongoose.connect('mongodb+srv://zabumaphu:2xAi9d3hUTFT2y2s@cluster0.azc0for.mongodb.net/')
+
+app.post('/register', async (req,res) => {
     const {username, password} = req.body;
-    res.json({requestData:{usename,password}});
+    const userDoc = await User.create({username, password});
+    res.json(userDoc);
 })
 
 app.listen(4000)
