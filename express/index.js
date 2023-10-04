96d562907f847a85a7e080773b371d44bf5ba626
aleksa <zabumaphu@gmail.com>
2023-10-03 23:14:02 -0400

    Backend authnetication done for login.

--- express/index.js
+++ express/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
-const User = require('./modules/user')
+const User = require('./modules/User')
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = '779966aaddffggbbww'
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors);
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://zabumaphu:2xAi9d3hUTFT2y2s@cluster0.azc0for.mongodb.net/')
-
-app.post('/register', async (req,res) => {
-    const {username, password} = req.body;
-    const userDoc = await User.create({username, password});
-    res.json(userDoc);
-})
-
 app.listen(4000)
+
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    //res.json(userDoc);
+    const passwordOk = bcrypt.compareSync(password, userDoc.password);
+    //res.json(passOk);
+    if (passOk){
+        // Logged in
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token',token).json('ok');
+        });
+        // res.json();
+    } else {
+        res.status(400).json('Try Harder');
+    }
+})
--- website/src/Pages/LoginPage/LoginPage.js
+++ website/src/Pages/LoginPage/LoginPage.js
@@ -2,18 +2,34 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
+import {useState} from "react";
 import "./LoginPage.css"
 
 export default function LoginPage() {
-  return (
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    <div className="page-container">
+  async function login(ev) {
+    ev.preventDefault();
+    await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type':'application/json'},
+    })
+  }
+
+  return (
+    <div className="page-container" onSubmit={login}>
     <div className="content-wrap">
           <Navbar/>
           <form action="" className='login'>
             <h2>Login</h2>
-            <input type='text' placeholder='username'/>
-            <input type='password' placeholder='password'/>
+            <input type='text'  placeholder='username' 
+                                value={username} 
+                                onChange={ev => setUsername(ev.target.value)}/>
+            <input type='password'  placeholder='password' 
+                                    value={password}
+                                    onChange={ev => setPassword(ev.target.value)}/>
             <button>Login</button>
           </form>
       </div>
