06d364c205008efffa098c7fa9e7277d3926d13a
aleksa <zabumaphu@gmail.com>
2023-10-26 23:55:33 -0400

    Registration functionality done.

--- express/index.js
+++ express/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./modules/User');
 
 app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
-    res.json('test ok2');
+mongoose.connect('mongodb+srv://zabumaphu:jwyPwYkOS5bwHPae@cluster0.kd2jcqv.mongodb.net/');
+// zabumaphu
+// jwyPwYkOS5bwHPae
+// mongodb+srv://zabumaphu:jwyPwYkOS5bwHPae@cluster0.kd2jcqv.mongodb.net/
+
+app.post('/register', async (req, res) => {
+    const {username, password} = req.body;
+    try{
+        const userDoc = await User.create({username, password})
+        res.json(userDoc);
+    } catch(e) {
+        res.status(400).json(e)
+    }
+
 });
 
 app.listen(4000);
