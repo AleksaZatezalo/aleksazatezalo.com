c504b2383df204656499f1d98108dcb8377f58d7
aleksa <zabumaphu@gmail.com>
2023-10-27 00:00:01 -0400

    Login functionality with bcrypt added.

--- express/index.js
+++ express/index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./modules/User');
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +18,8 @@ mongoose.connect('mongodb+srv://zabumaphu:jwyPwYkOS5bwHPae@cluster0.kd2jcqv.mong
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
     try{
-        const userDoc = await User.create({username, password})
+        const userDoc = await User.create({username, 
+            password:bcrypt.hashSync(password, salt)})
         res.json(userDoc);
     } catch(e) {
         res.status(400).json(e)
