37c6fecd5e5c5b13c9abadbfa9d11b93e5104248
aleksa <zabumaphu@gmail.com>
2023-10-29 00:54:54 -0400

    Viewing posts finished.

--- website/src/Pages/PostPage/PostPage.js
+++ website/src/Pages/PostPage/PostPage.js
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import Navbar from "../../Components/Navbar/Navbar";
+import Footer from "../../Components/Footer/Footer";
+import {formatISO9075} from "date-fns"
+
 
 export default function PostPage(){
     const [postInfo, setPostInfo] = useState(null);
@@ -12,7 +16,22 @@ export default function PostPage(){
             });
         });
     }, []);
+
+    if (!postInfo) return '';
+
     return (
-        <h1>PostPage</h1>
-    )
+    <div className="page-container">
+        <div className="content-wrap">
+            <Navbar/>
+            <div className="post-page">
+                <h1>{postInfo.title}</h1>
+                <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+                <div style={{paddingLeft:'10rem'}} className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
+            </div>
+        </div>
+        <Footer/>
+    </div>
+    
+        
+    );
 }
