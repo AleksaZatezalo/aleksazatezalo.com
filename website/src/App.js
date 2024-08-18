54aeff33d618e7bb2ed49f8390aa6a16f51bb6d6
aleksa <zabumaphu@gmail.com>
2024-08-18 16:58:01 -0400

    Fat trimmed. Basic UI Site Only.

--- website/src/App.js
+++ website/src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import BlogPage from "./Pages/BlogPage/BlogPage"
 import HomePage from './Pages/AboutPage/HomePage';
 import AboutPage from "./Pages/AboutPage/AboutPage"
-import LoginPage from './Pages/LoginPage/LoginPage';
-import CreatePost from './Pages/CreatePage/CreatePost';
-import EditPost from './Pages/EditPost/EditPost';
 import { UserContextProvider } from './UserContext';
 import './App.css';
-import PostPage from './Pages/PostPage/PostPage';
 
 function App() {
   return (
@@ -19,12 +14,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route index element={<HomePage/>} />
-              <Route path="/blog" index element={<BlogPage/>} />
               <Route path="/about" index element={<AboutPage/>} />
-              <Route path="/login" index element={<LoginPage/>}/>
-              <Route path="/create" index element={<CreatePost/>}/>
-              <Route path="/post/:id" index element={<PostPage/>}/>
-              <Route path="/edit/:id" element={<EditPost />} />
             </Routes>
           </BrowserRouter>
         </UserContextProvider>
--- website/src/Components/Navbar/Navbar.js
+++ website/src/Components/Navbar/Navbar.js
@@ -4,44 +4,12 @@ import {UserContext} from "../../UserContext";
 
 function Navbar() {
     const navRef = useRef();
-    const {setUserInfo,userInfo} = useContext(UserContext);
-    useEffect(() => {
-        fetch('http://localhost:4000/profile', {
-            credentials: 'include',
-        }).then(response => {
-            response.json().then(userInfo => {
-            setUserInfo(userInfo);
-            });
-        });
-    }, []);
-
-function logout() {
-    fetch('http://localhost:4000/logout', {
-      credentials: 'include',
-      method: 'POST',
-    });
-  }
-
-  const username = userInfo?.username;
 
     return (
         <header>
             <h3>Welcome to Aleksa's Website</h3>
             <nav ref={navRef}>
                 <a href ="/about">About Aleksa</a> 
-                <a href ="/blog">Blog</a>
-                {!username && (
-                    <>
-                        <a href ="/login">Login</a>
-                    </>
-                )}
-                {username && (
-                    <>
-                        <a href ="/create">Add Post</a>
-                        <a href="/blog" onClick={logout}>Logout</a>
-                    </>
-                )}
-                
             </nav>
             
         </header>
